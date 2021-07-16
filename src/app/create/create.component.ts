bf1f071f7e7ff60c19102003a8b11a0baf37c454
Kaloyan Daskalov <kdaskalov78@gmail.com>
2021-07-16 18:27:48 +0300

    adding reactive form control on create form

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faHome,faDollarSign, faImage, faMap, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,8 +8,11 @@ import { faHome,faDollarSign, faImage, faMap, faInfoCircle } from '@fortawesome/
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-  @ViewChild('f') formData!: NgForm;
-  faHome= faHome;
+  types: string[]= ['house', 'flat', 'office', 'land' ];
+  imagePattern : RegExp = /^https?:\/\//;
+  canSubmit: boolean = false;
+  formData!: FormGroup;
+  faHome = faHome;
   faDollarSign = faDollarSign;
   faImage = faImage;
   faMap = faMap;
@@ -18,10 +21,28 @@ export class CreateComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.formData = new FormGroup({
+      title: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+      price: new FormControl(null,[Validators.required, Validators.min(1), Validators.max(5000000)]),
+      imageUrl: new FormControl(null, [Validators.required, this.checkImage.bind(this)]),
+      address: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+      type: new FormControl(null, [Validators.required]),
+      description: new FormControl(null, [Validators.required, Validators.minLength(20), Validators.maxLength(500)])
+    });
+    this.formData.statusChanges.subscribe(status => {
+      if(status === 'VALID') this.canSubmit = true;
+    });
+  }
+
+  checkImage(control: FormControl): {[key:string]: boolean} | null {
+    if (!this.imagePattern.test(control.value)) {
+      return {'invalidImageUrl': true}
+    }
+
+    return null;
   }
 
   onSubmit () {
-    console.log(this.formData.form);
-    
+    console.log(this.formData);
   }
 }
