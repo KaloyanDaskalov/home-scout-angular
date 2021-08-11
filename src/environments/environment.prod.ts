dda212b5846a57f526f48d34984edbd2438b4ffa
Kaloyan Daskalov <kdaskalov78@gmail.com>
2021-08-11 15:18:59 +0300

    prepearing project for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAklHL6MDSoEiGOH7JzzVY7MNwQuEcUd-M",
+    authDomain: "home-scout-76bf8.firebaseapp.com",
+    databaseURL: "https://home-scout-76bf8-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "home-scout-76bf8",
+    storageBucket: "home-scout-76bf8.appspot.com",
+    messagingSenderId: "385072287552",
+    appId: "1:385072287552:web:c99a8e1cbc6e56ad73f4a5"
+
+  }
 };
